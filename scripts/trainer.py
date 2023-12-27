7681be1a8d95960c8c0e7892a7386ac1377d3b93
Jordach <jordach@wolfo.tech>
2023-12-27 19:57:30 +0000

    Text encoder outputs cannot be moved

--- scripts/trainer.py
+++ scripts/trainer.py
@@ -2031,7 +2031,7 @@ def main():
                                     encode = accelerator.unwrap_model(text_encoder)(chunk, output_hidden_states=True)
                                 else:
                                     encode = text_encoder(chunk, output_hidden_states=True)
-                                encode = encode.to(accelerator.device)
+                                
                                 if z is None:
                                     if args.clip_penultimate:
                                         if args.multi_gpu:
