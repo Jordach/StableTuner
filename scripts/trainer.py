2f4f5f7f9e10bb44a78f78f7833a56ce4b0cee27
Jordach <jordach@wolfo.tech>
2024-01-06 07:15:59 +0000

    Stop moving the TE around so much

--- scripts/trainer.py
+++ scripts/trainer.py
@@ -2055,8 +2055,7 @@ def main():
                                     break
 
                                 chunk = chunk.to(accelerator.device)
-                                chunk = torch.cat((torch.full((chunk.shape[0], 1), tokenizer.bos_token_id).to(accelerator.device), chunk, torch.full((chunk.shape[0], 1), tokenizer.eos_token_id).to(accelerator.device)), 1)
-                                text_encoder = text_encoder.to(accelerator.device)
+                                chunk = torch.cat((torch.full((chunk.shape[0], 1), tokenizer.bos_token_id).to(accelerator.device), chunk, torch.full((chunk.shape[0], 1), tokenizer.eos_token_id).to(accelerator.device)), 1))
                                 encode = ""
                                 
                                 if args.multi_gpu:
